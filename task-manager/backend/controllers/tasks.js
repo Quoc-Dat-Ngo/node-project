ed5bf56c6b6ab1d28df3a702d6f58afb05ced157
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-09 14:30:35 +1100

    Add controller function and router for creating new task route

--- task-manager/backend/controllers/tasks.js
+++ task-manager/backend/controllers/tasks.js
@@ -2,6 +2,11 @@ const getAllTasks = (req, res) => {
   res.send('All items from the database');
 };
 
+const createTask = (req, res) => {
+  res.send('New Task');
+};
+
 module.exports = {
   getAllTasks,
+  createTask,
 };
