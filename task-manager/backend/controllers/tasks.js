33cc77938669920445ebfbd5818bf080dfe63516
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-09 14:40:35 +1100

    Add controller function and router for get a single task, update a single task andelete a single task

--- task-manager/backend/controllers/tasks.js
+++ task-manager/backend/controllers/tasks.js
@@ -6,7 +6,25 @@ const createTask = (req, res) => {
   res.send('New Task');
 };
 
+const getSingleTask = (req, res) => {
+  const { id } = req.params;
+  res.send('Info about the selected task');
+};
+
+const updateSingleTask = (req, res) => {
+  const { id } = req.params;
+  res.send('Update the selected task');
+};
+
+const deleteSingleTask = (req, res) => {
+  const { id } = req.params;
+  res.send('Delete the selected task');
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getSingleTask,
+  updateSingleTask,
+  deleteSingleTask,
 };
