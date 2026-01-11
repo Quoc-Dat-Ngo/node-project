12bfdd20ceeb167ee613eee479a376592da50dd3
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-11 14:22:15 +1100

    Refactor code, use pool.query to write simple queries

--- task-manager/backend/database/init.js
+++ task-manager/backend/database/init.js
@@ -1,19 +1,10 @@
-const _ = require('dotenv/config');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-  },
-});
+const pool = require('./pool');
 
 const init_db = async () => {
-  const client = await pool.connect();
   try {
-    await client.query('DROP TABLE IF EXISTS tasks;');
+    await pool.query('DROP TABLE IF EXISTS tasks;');
 
-    await client.query(`
+    await pool.query(`
     	CREATE TABLE IF NOT EXISTS tasks (
     		id SERIAL PRIMARY KEY,
     		title VARCHAR(255) NOT NULL,
@@ -23,9 +14,6 @@ const init_db = async () => {
     console.log('Create new table for managing tasks');
   } catch (e) {
     console.error('Fail to initialise database', e);
-  } finally {
-    client.release();
-    pool.end();
   }
 };
 
