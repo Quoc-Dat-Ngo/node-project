59fb8e1e91f2e28b2bdd340aa2aadf3377ed7cf2
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-12 12:30:26 +1100

    Add raw SQL query to extract a single row/record

--- task-manager/backend/database/read_tasks.js
+++ task-manager/backend/database/read_tasks.js
@@ -5,8 +5,26 @@ const read_all_tasks = async () => {
     const { rows } = await pool.query('SELECT * FROM tasks');
     return rows;
   } catch (e) {
-    console.error('Fail to retrieve all active tasks', e);
+    console.error('Fail to retrieve all active tasks.', e);
   }
 };
 
-module.exports = { read_all_tasks };
+const read_single_task = async id => {
+  try {
+    // Method 1 (Indirect):
+    // const tasks = await read_all_tasks();
+    // console.log(tasks);
+    // console.log(id);
+    // const task_to_return = tasks.find(task => task.id === id);
+
+    // Method 2 (Direct):
+    const { rows } = await pool.query('Select * FROM tasks WHERE id = $1', [
+      id,
+    ]);
+    return rows;
+  } catch (e) {
+    console.error(`Fail to retrieve the task with given id ${id}`);
+  }
+};
+
+module.exports = { read_all_tasks, read_single_task };
