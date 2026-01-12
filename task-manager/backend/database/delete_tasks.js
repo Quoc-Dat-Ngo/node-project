5661b076a932b3ffee0bd20b864f7a25260fb0fa
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-12 15:18:19 +1100

    Write raw SQL query to remove a record/row from table 'tasks' with given 'id'

--- task-manager/backend/database/delete_tasks.js
+++ task-manager/backend/database/delete_tasks.js
@@ -1,5 +1,12 @@
 const pool = require('./pool');
 
-const delete_single_task = async id => {};
+const delete_single_task = async id => {
+  try {
+    const del = await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
+    return del;
+  } catch (e) {
+    console.error(`Fail to delete a task with given id ${id}`, e);
+  }
+};
 
 module.exports = delete_single_task;
