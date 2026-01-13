e72b6b9119875be307e5365f367dfa54615d931a
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-13 18:01:08 +1100

    Fix typo of status, correct version is statusCode

--- task-manager/backend/middleware/errorHandler.js
+++ task-manager/backend/middleware/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  const status = err.status || 500;
+  const status = err.statusCode || 500;
 
   res.status(status).json({
     sucess: false,
